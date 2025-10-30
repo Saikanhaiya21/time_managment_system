ffd918de05cfa2080446fd9663f541459c9c2f94
Saikanhaiya21 <80808406+Saikanhaiya21@users.noreply.github.com>
2025-10-30 09:17:13 +0000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+users = [
+          { name: 'Rakesh', preferred_working_hour_per_day: 7.5 },
+          { name: 'Umesh', preferred_working_hour_per_day: 7.5 }
+        ]
+User.create(users)
+
+worklogs = [
+  { user_id: 1, date: Date.new(2021,01,20), hours: 7.5, notes: 'Project 1 work'},
+  { user_id: 1, date: Date.new(2021,01,21), hours: 5.5, notes: 'Project 2 work'},
+  { user_id: 1, date: Date.new(2021,01,22), hours: 2.5, notes: 'Project 3 work'},
+  { user_id: 2, date: Date.new(2021,01,20), hours: 4, notes: 'Project 4 work'},
+  { user_id: 2, date: Date.new(2021,01,21), hours: 3, notes: 'Project 5 work'},
+  { user_id: 2, date: Date.new(2021,01,22), hours: 4.5, notes: 'Project 6 work'}
+]
+
+Worklog.create(worklogs)
+
